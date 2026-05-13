79f261219f6218eb4331d085fae57df613810e2f
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2026-05-13 20:21:05 +0100

    Allow cat to recieve a series of files and concatenate them

--- src/bin/cat.ts
+++ src/bin/cat.ts
@@ -1,5 +1,13 @@
 import { Environment } from "../types/worker";
 
-export default async function* cat(env: Environment, [path]: [string]) {
-	env.print((await env.fs.readFile(path)) ?? "");
+export default async function* cat(env: Environment, ...paths: string[]) {
+	const strings = await Promise.all(
+		paths.map(async (path) => {
+			const contents = await env.fs.readFile(path);
+
+			return contents ?? "";
+		})
+	);
+
+	env.print(strings.join("\n"));
 }
