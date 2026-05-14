7685488be638cf6b39f673e62867b9a764181524
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2026-05-14 08:04:56 +0100

    Fix: cat cannot read file because path must be string

--- src/bin/cat.ts
+++ src/bin/cat.ts
@@ -1,6 +1,6 @@
 import { Environment } from "../types/worker";
 
-export default async function* cat(env: Environment, ...paths: string[]) {
+export default async function* cat(env: Environment, paths: string[]) {
 	const strings = await Promise.all(
 		paths.map(async (path) => {
 			const contents = await env.fs.readFile(path);
