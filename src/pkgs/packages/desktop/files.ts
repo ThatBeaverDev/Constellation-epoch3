dee9655e0c45b8d2b456e703102eb041139a5ac3
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2026-07-22 14:17:07 +0100

    Add basic rows and a header stating current path

--- src/pkgs/packages/desktop/files.ts
+++ src/pkgs/packages/desktop/files.ts
@@ -2,6 +2,17 @@ import { Environment } from "../../../util/types/worker";
 import GraphicalUIManager from "../gui/lib.gui";
 import { WindowContentItem } from "../gui/types/windowContents";
 
+function header(width: number, path: string) {
+	const height = 50;
+
+	const contents: WindowContentItem[] = [
+		{ type: "box", x: 0, y: 0, width, height },
+		{ type: "text", x: 5, y: 5, text: path }
+	];
+
+	return { height, contents };
+}
+
 export default async function* FilesApp(env: Environment) {
 	const lib = new GraphicalUIManager(env);
 	await lib.init("Files");
@@ -12,11 +23,27 @@ export default async function* FilesApp(env: Environment) {
 		const contents = await env.fs.readdir(path);
 
 		const ui: WindowContentItem[] = [];
+		const { height: headerHeight, contents: headerContents } = header(
+			lib.dimensions.width,
+			path
+		);
+		ui.push(...headerContents);
 
-		let y = 5;
+		let y = 5 + headerHeight;
 
 		for (const item of contents) {
-			ui.push({ type: "text", text: item, x: 5, y: (y += 20) });
+			const row: WindowContentItem[] = [
+				{
+					type: "box",
+					x: 5,
+					y: (y += 50),
+					width: lib.dimensions.width - 20,
+					height: 40
+				},
+				{ type: "text", text: item, x: 5, y: y + 5 }
+			];
+
+			ui.push(...row);
 		}
 
 		lib.setContents(ui);
