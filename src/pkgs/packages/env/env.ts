7c76897feb62e969f7ebde23e83fe4acfce1c1d1
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2026-07-22 23:18:47 +0100

    Allow `env` to handle being provided paths

--- src/pkgs/packages/env/env.ts
+++ src/pkgs/packages/env/env.ts
@@ -6,14 +6,28 @@ export default async function* ResolveCommand(
 ) {
 	const PATH = ["/bin"];
 
+	if ([".", "/"].includes(name[0])) {
+		// resolve it
+		const path = env.path.resolve(env.workingDirectory, name);
+
+		const exists = await env.fs.exists(path);
+		if (exists) {
+			return path;
+		}
+	}
+
 	for (const dir of PATH) {
-		const contents = await env.fs.readdir(dir);
+		try {
+			const contents = await env.fs.readdir(dir);
 
-		const filename = `${name}.js`;
-		const searchPath = `${dir}/${filename}`;
+			const filename = `${name}.js`;
+			const searchPath = `${dir}/${filename}`;
 
-		if (contents.includes(filename)) {
-			return searchPath;
+			if (contents.includes(filename)) {
+				return searchPath;
+			}
+		} catch (e) {
+			console.warn(e);
 		}
 	}
 }
