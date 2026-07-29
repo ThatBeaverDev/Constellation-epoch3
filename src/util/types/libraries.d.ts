8a9ff8c81ef7aa46f5454102043c17e0377284ea
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2026-07-29 15:16:55 +0100

    Move `rotur` library to package

--- src/util/types/libraries.d.ts
+++ src/util/types/libraries.d.ts
@@ -1,5 +1,13 @@
 import GuiWindow from "../../pkgs/packages/lib-gui/lib-gui";
+import RoturLibrary, {
+	RoturLibraryConstructor
+} from "../../pkgs/packages/lib-rotur/lib-rotur";
+import { type getRoturToken } from "../../pkgs/packages/lib-rotur/lib-rotur";
 
 export interface LibraryExports {
 	"lib-gui": { default: typeof GuiWindow };
+	"lib-rotur": {
+		default: RoturLibraryConstructor;
+		getRoturToken: getRoturToken;
+	};
 }
